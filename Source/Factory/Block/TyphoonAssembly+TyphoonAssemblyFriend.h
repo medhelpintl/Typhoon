c2e519073b0e65d87fba390f3d91f5483b530c7a
Scott Fisher <scottfisher@Mais-MacBook-Pro.local>
2014-07-08 18:10:26 -0700

    Not sure what i did here...

---
+++ Source/Factory/Block/TyphoonAssembly+TyphoonAssemblyFriend.h
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  TYPHOON FRAMEWORK
+//  Copyright 2013, Jasper Blues & Contributors
+//  All Rights Reserved.
+//
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#import <Foundation/Foundation.h>
+#import "TyphoonAssembly.h"
+#import "TyphoonDefinition.h"
+
+@interface TyphoonAssembly (TyphoonAssemblyFriend)
+
++ (BOOL)selectorReservedOrPropertySetter:(SEL)selector;
+
+- (void)prepareForUse;
+
+- (NSArray *)definitions;
+
+- (TyphoonDefinition *)definitionForKey:(NSString *)key;
+
+@property(readonly) NSSet *definitionSelectors;
+
+@end
